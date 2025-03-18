edc00bbc398924e0536ad6e3500474f985b25ada
Charles <charles-iacopino@hotmail.com>
2025-03-18 12:40:45 +0100

    Fizzbuzz fonctionnel

--- src/main/java/fr/anthonyquere/fizzbuzz/FizzBuzz.java
+++ src/main/java/fr/anthonyquere/fizzbuzz/FizzBuzz.java
@@ -1,5 +1,6 @@
 package fr.anthonyquere.fizzbuzz;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FizzBuzz {
@@ -9,6 +10,20 @@ public class FizzBuzz {
     }
 
     public static List<String> startFizzBuzz(int count) {
-        return null;
+        List<String> la_liste = new ArrayList<String>();
+
+        for(int index = 1; index < count + 1; index++){
+            String str_to_add = "";
+            if(index%3 == 0){
+                str_to_add += "Fizz";
+            }
+            if(index%5 == 0){
+                str_to_add += "Buzz";
+            }
+            // System.out.println(str_to_add);
+            la_liste.add(str_to_add);
+        }
+
+        return la_liste;
     }
 }
